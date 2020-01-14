477edf1beb60e8e53bdf5bbae4ad058216cc9e14
Marlon Lira <marlonlira3@gmail.com>
2020-01-14 14:39:52 -0300

    feat(template, routes, components): Adicionado o template adminlte
    Configurado o template Admin lte, router, dashboard e outros
    componentes.
    
    também foi configurado o redux e requisições via o axios.

---
+++ frontend/src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise';
+
+import App from './main/app';
+import Reducers from './main/reducers';
+
+const store = applyMiddleware(promise)(createStore)(Reducers);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'));
